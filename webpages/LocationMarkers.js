3cd079e8317fac2ef26be31d4ab9c405e97e9f6d
Siam Salam <82978084+siam-s@users.noreply.github.com>
2024-03-24 04:14:07 -0400

    Checkbox addition to MapPage, made new JS for data, and added more styling in general

---
+++ webpages/LocationMarkers.js
@@ -0,0 +1,37 @@
+var al_carbon_m = L.marker([38.086820, -78.469880]).addTo(map);
+var alley_light_m = L.marker([38.030520, -78.482330]);
+var beer_run_m = L.marker([38.024630, -78.468520]);
+var belle_m = L.marker([38.0248971, -78.475415]);
+var birdhouse_m = L.marker([38.0381604, -78.4845472]).addTo(map);
+var bizou_m = L.marker([38.0310719, -78.4820607]);
+var bodos_m = L.marker([38.0561607, -78.4965435]);
+var broadcloth_m = L.marker([38.0214936, -78.4607717]); //
+var cno_m = L.marker([38.0252185, -78.4696998]);
+var cafe_frank_m = L.marker([38.03062709012528, -78.47951430704089]);
+var conmole_m = L.marker([38.024763487802616, -78.47578686175572]);
+var crushpad_m = L.marker([38.03096363152615, -78.482064459906]);
+var luce_m = L.marker([38.03151803082232, -78.48198057524853]);
+var fleurie_m = L.marker([38.03074006288903, -78.47972247602803]).addTo(map);
+var ivy_inn_m = L.marker([38.046030361539245, -78.51623666175475]);
+var lampo_m = L.marker([38.02756505114911, -78.47688086175562]);
+var mas_m = L.marker([38.024439307801075, -78.47412497339937]);
+var milan_indian_m = L.marker([38.060764768124734, -78.49330967524709]);
+var oakhart_social_m = L.marker([38.0314250375689, -78.48807223291978]);
+var pad_thai_m = L.marker([38.02481230366002, -78.46848020408447]);
+var peter_chang_m = L.marker([38.05543382524484, -78.50056586175427]);
+var petit_pois_m = L.marker([38.030969240340134, -78.48033249059105]);
+var public_fish_m = L.marker([38.03145079000524, -78.48822351942677]);
+var revolutionary_soup_m = L.marker([38.03082383888679, -78.48233750408416]);
+var south_central_m = L.marker([38.03841532210556, -78.49152958874129]);
+var sultan_kebab_m = L.marker([38.0287164503935, -78.48135731942685]);
+var tavola_m = L.marker([38.024710303895304, -78.47534271757756]);
+var ten_m = L.marker([38.03064239205715, -78.48101040408419]);
+var zocalo_m = L.marker([38.03100304379837, -78.48036467709792]);
+
+var union_station_m = L.marker([38.03149962596708, -78.49182173291983]);
+
+var monticello_m = L.marker([38.0085937195138, -78.453211560453]);
+var albemarle_county_circuit_court_m = L.marker([38.031647883820426, -78.47745113745796]);
+var belmont_m = L.marker([38.024444, -78.4775]);
+var albemarle_historical_society_m = L.marker([38.031795495390426, -78.4799721]);
+zocalo_m.addTo(map);
