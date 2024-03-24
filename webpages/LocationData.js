3c0a90210f3790d97a2b6e052ee653996ac86d5a
Siam Salam <82978084+siam-s@users.noreply.github.com>
2024-03-24 05:48:40 -0400

    Working implementation of map with built-in layers

--- webpages/LocationData.js
+++ webpages/LocationData.js
@@ -7,34 +7,34 @@ function Restaurant(rName, rDescription, rAddress, rPhone, price) {
     this.price = price;
 }
 
-var al_carbon = new Restaurant("Al Carbon Chicken", "Tacos, tamales and sandwiches with rotisserie chicken", "1871 Seminole Trail", "964-1052",  "$");
-var alley_light = new Restaurant("The Alley Light", "Intimate small-plate spot above Revolutionary Soup", "108 Second St. SW", "296-5003", "$$");
-var beer_run = new Restaurant("Beer Run", "Rotating beers on tap, six-packs and wine to take away and three meals daily", "156 Carlton Rd., Suite 203", "984-2337", "$$");
-var belle = new Restaurant("Belle", "A café, sandwich shop and bakery, the Shanesy brothers’ homage to their love of food", "407 Monticello Rd", "328-8755", "$$");
-var birdhouse = new Restaurant("Birdhouse", "Farm-to-table food, simply prepared, in the coziest room in town", "711 Henry Ave", "443-0696", "$$");
-var bizou = new Restaurant("Bizou", "Upscale down-home cookin’ with fresh fish, pork and beef dishes", "119 W. Main St., Downtown Mall", "977-1818", "$$");
-var bodos = new Restaurant("Bodo's Bagels", "Still the king of bagels in our town. ", "1418 N. Emmet St.", "977-9598", "$");
-var broadcloth = new Restaurant("Broadcloth", "Modern cuisine in an elegant room in a historic textile mill", "1837 Broadway St.", "270-0555", "$$$");
-var cno = new Restaurant("C&O", "Sophisticated French at a 30-plus-year-old establishment. Excellent cheese plate, extensive wine list, popular bar.", "515 E. Water St.", "971-7044", "$$$");
-var cafe_frank = new Restaurant("Cafe Frank", "Upscale-casual Franco-Italian restaurant and cocktail bar on Downtown Mall", "317 E Main St.", "825-9496", "$$$");
-var conmole = new Restaurant("Conmole", "With traditional family recipes, an escape to Oaxaca in Belmont", "816 Hinton Ave.", "328-2519", "$$");
-var crushpad = new Restaurant("Crushpad", "Charlottesville’s wine Mecca.", "118 West Main St. Suite B.", "284-7119", "$$");
-var luce = new Restaurant("Luce", "Authentic Italian pizza with great crust and homemade pasta.", "110 2nd St. NW.", "No number!", "$$");
-var fleurie = new Restaurant("Fleurie", "Upscale modern French restaurant gets rave reviews. Extensive wine list.", "108 Third St. NE.", "971-7800", "$$$");
-var ivy_inn = new Restaurant("Ivy Inn", "Daily menu of modern American cuisine in an 18th century tollhouse.", "2244 Old Ivy Rd", "977-1222", "$$$");
-var lampo = new Restaurant("Lampo", "Authentic Neapolitan pizzeria in Belmont.", "205 Monticello Rd.", "282-0607", "$$");
-var mas = new Restaurant("Mas", "Authentic Spanish tapas and wines in a funky, dimly lit atmosphere in the heart of Belmont", "904 Monticello Rd", "979-0990", "$$");
-var milan_indian = new Restaurant("Milan Indian Cuisine", "Authentic Indian cuisine with all the standards; daily lunch buffet.", "1817 Emmet St.", "984-2828", "$$");
-var oakhart_social = new Restaurant("Oakhart Social", "Seasonal Atlantic Coast food for sharing.", "511 W. Main St.", "995-5449", "$$");
-var pad_thai = new Restaurant("Pad Thai", "Homestyle Thai cooking from an experienced chef.", "156 Carlton Rd.", "293-4032", "$$");
-var peter_chang = new Restaurant("Peter Chang China Grill", "Authentic Sichuan cuisine from a renowned chef.", "2162 Barracks Rd.", "244-9818", "$$");
-var petit_pois = new Restaurant("Petit Pois", "French-American bistro from the owners of Fleurie. All the chic, lower check.", "201 E. Main St.", "979-7647", "$$");
-var public_fish = new Restaurant("Public Fish & Oyster", "Simply prepared, responsibly sourced seafood. Shucked oysters, raw bar and a full bar.", "513 W. Main St.", "995-5542", "$$");
-var revolutionary_soup = new Restaurant("Revolutionary Soup", "Choose from a slew of enticing soups made daily.", "108 Second St. SW.", "296-7687", "$");
-var south_central = new Restaurant("South and Central", "The Dairy Market’s airy Latin American steakhouse", "946 Grady Ave.", "202-1945", "$$$");
-var sultan_kebab = new Restaurant("Sultan Kebab", "Authentic Turkish food, all kind of kebabs, vegetarian dishes, salads, homemade Turkish baklava, Turkish tea and coffee.", "333 Second St. SE.", "981-0090", "$");
-var ten = new Restaurant("Ten", "Super swanky second-floor spot serving modern Japanese", "120B E. Main St., Downtown Mall", "295-6691", "$$");
-var zocalo = new Restaurant("Zocalo", "Flavorful high-end, Latin-inspired cuisine with a full bar.", "201 E. Main St., Downtown Mall", "977-4944", "$$");
+var nal_carbon = new Restaurant("Al Carbon Chicken", "Tacos, tamales and sandwiches with rotisserie chicken", "1871 Seminole Trail", "964-1052",  "$"),
+alley_light = new Restaurant("The Alley Light", "Intimate small-plate spot above Revolutionary Soup", "108 Second St. SW", "296-5003", "$$"),
+beer_run = new Restaurant("Beer Run", "Rotating beers on tap, six-packs and wine to take away and three meals daily", "156 Carlton Rd., Suite 203", "984-2337", "$$"),
+belle = new Restaurant("Belle", "A café, sandwich shop and bakery, the Shanesy brothers’ homage to their love of food", "407 Monticello Rd", "328-8755", "$$"),
+birdhouse = new Restaurant("Birdhouse", "Farm-to-table food, simply prepared, in the coziest room in town", "711 Henry Ave", "443-0696", "$$"),
+bizou = new Restaurant("Bizou", "Upscale down-home cookin’ with fresh fish, pork and beef dishes", "119 W. Main St., Downtown Mall", "977-1818", "$$"),
+bodos = new Restaurant("Bodo's Bagels", "Still the king of bagels in our town. ", "1418 N. Emmet St.", "977-9598", "$"),
+broadcloth = new Restaurant("Broadcloth", "Modern cuisine in an elegant room in a historic textile mill", "1837 Broadway St.", "270-0555", "$$$"),
+cno = new Restaurant("C&O", "Sophisticated French at a 30-plus-year-old establishment. Excellent cheese plate, extensive wine list, popular bar.", "515 E. Water St.", "971-7044", "$$$"),
+cafe_frank = new Restaurant("Cafe Frank", "Upscale-casual Franco-Italian restaurant and cocktail bar on Downtown Mall", "317 E Main St.", "825-9496", "$$$"),
+conmole = new Restaurant("Conmole", "With traditional family recipes, an escape to Oaxaca in Belmont", "816 Hinton Ave.", "328-2519", "$$"),
+crushpad = new Restaurant("Crushpad", "Charlottesville’s wine Mecca.", "118 West Main St. Suite B.", "284-7119", "$$"),
+luce = new Restaurant("Luce", "Authentic Italian pizza with great crust and homemade pasta.", "110 2nd St. NW.", "No number!", "$$"),
+fleurie = new Restaurant("Fleurie", "Upscale modern French restaurant gets rave reviews. Extensive wine list.", "108 Third St. NE.", "971-7800", "$$$"),
+ivy_inn = new Restaurant("Ivy Inn", "Daily menu of modern American cuisine in an 18th century tollhouse.", "2244 Old Ivy Rd", "977-1222", "$$$"),
+lampo = new Restaurant("Lampo", "Authentic Neapolitan pizzeria in Belmont.", "205 Monticello Rd.", "282-0607", "$$"),
+mas = new Restaurant("Mas", "Authentic Spanish tapas and wines in a funky, dimly lit atmosphere in the heart of Belmont", "904 Monticello Rd", "979-0990", "$$"),
+milan_indian = new Restaurant("Milan Indian Cuisine", "Authentic Indian cuisine with all the standards; daily lunch buffet.", "1817 Emmet St.", "984-2828", "$$"),
+oakhart_social = new Restaurant("Oakhart Social", "Seasonal Atlantic Coast food for sharing.", "511 W. Main St.", "995-5449", "$$"),
+pad_thai = new Restaurant("Pad Thai", "Homestyle Thai cooking from an experienced chef.", "156 Carlton Rd.", "293-4032", "$$"),
+peter_chang = new Restaurant("Peter Chang China Grill", "Authentic Sichuan cuisine from a renowned chef.", "2162 Barracks Rd.", "244-9818", "$$"),
+petit_pois = new Restaurant("Petit Pois", "French-American bistro from the owners of Fleurie. All the chic, lower check.", "201 E. Main St.", "979-7647", "$$"),
+public_fish = new Restaurant("Public Fish & Oyster", "Simply prepared, responsibly sourced seafood. Shucked oysters, raw bar and a full bar.", "513 W. Main St.", "995-5542", "$$"),
+revolutionary_soup = new Restaurant("Revolutionary Soup", "Choose from a slew of enticing soups made daily.", "108 Second St. SW.", "296-7687", "$"),
+south_central = new Restaurant("South and Central", "The Dairy Market’s airy Latin American steakhouse", "946 Grady Ave.", "202-1945", "$$$"),
+sultan_kebab = new Restaurant("Sultan Kebab", "Authentic Turkish food, all kind of kebabs, vegetarian dishes, salads, homemade Turkish baklava, Turkish tea and coffee.", "333 Second St. SE.", "981-0090", "$"),
+ten = new Restaurant("Ten", "Super swanky second-floor spot serving modern Japanese", "120B E. Main St., Downtown Mall", "295-6691", "$$"),
+zocalo = new Restaurant("Zocalo", "Flavorful high-end, Latin-inspired cuisine with a full bar.", "201 E. Main St., Downtown Mall", "977-4944", "$$");
 
 function Transportation(tName, tDescription, tAddress, tPhone) {
     this.tName = tName;
@@ -43,7 +43,7 @@ function Transportation(tName, tDescription, tAddress, tPhone) {
     this.tPhone = tPhone;
 }
 
-var union_station = new Transportation("Charlottesville Union Station (CVS)", "Train station that is served by Amtrak, Crescent, and Northeast Regional. Opened in 1885.", "810 W Main St", "872-7245");
+union_station = new Transportation("Charlottesville Union Station (CVS)", "Train station that is served by Amtrak, Crescent, and Northeast Regional. Opened in 1885.", "810 W Main St", "872-7245");
 
 
 function Historical(hName, hDescription, hAddress, hPhone) {
@@ -53,6 +53,6 @@ function Historical(hName, hDescription, hAddress, hPhone) {
     this.hPhone = hPhone;
 }
 
-var monticello = new Historical("Monticello", "President Jefferson's stately 1769 mansion & slave plantation, with tours, a museum, a cafe & more.", "1050 Monticello Loop", "984-9800");
-var albemarle_county_circuit_court = new Historical("Albemarle County Circuit Court", "Originally built in 1762 by William Cabell and then rebuilt in 1803.", "501 E Jefferson St.", "972-4083");
-var belmont = new Historical("Belmont", "Also known as the Ficklin Mansion.", "501 E Jefferson St.", "972-4083");
+var monticello = new Historical("Monticello", "President Jefferson's stately 1769 mansion & slave plantation, with tours, a museum, a cafe & more.", "1050 Monticello Loop", "984-9800"),
+albemarle_county_circuit_court = new Historical("Albemarle County Circuit Court", "Originally built in 1762 by William Cabell and then rebuilt in 1803.", "501 E Jefferson St.", "972-4083"),
+belmont = new Historical("Belmont", "Also known as the Ficklin Mansion.", "501 E Jefferson St.", "972-4083");
--- webpages/MapScript.js
+++ webpages/MapScript.js
@@ -1,6 +1,86 @@
-var map = L.map("map").setView([38.0293, -78.4767], 10);
+var al_carbon_m = L.marker([38.086820, -78.469880]),
+alley_light_m = L.marker([38.030520, -78.482330]),
+beer_run_m = L.marker([38.024630, -78.468520]),
+belle_m = L.marker([38.0248971, -78.475415]),
+birdhouse_m = L.marker([38.0381604, -78.4845472]),
+bizou_m = L.marker([38.0310719, -78.4820607]),
+bodos_m = L.marker([38.0561607, -78.4965435]),
+broadcloth_m = L.marker([38.0214936, -78.4607717]), //
+cno_m = L.marker([38.0252185, -78.4696998]),
+cafe_frank_m = L.marker([38.03062709012528, -78.47951430704089]),
+conmole_m = L.marker([38.024763487802616, -78.47578686175572]),
+crushpad_m = L.marker([38.03096363152615, -78.482064459906]),
+luce_m = L.marker([38.03151803082232, -78.48198057524853]),
+fleurie_m = L.marker([38.03074006288903, -78.47972247602803]),
+ivy_inn_m = L.marker([38.046030361539245, -78.51623666175475]),
+lampo_m = L.marker([38.02756505114911, -78.47688086175562]),
+mas_m = L.marker([38.024439307801075, -78.47412497339937]),
+milan_indian_m = L.marker([38.060764768124734, -78.49330967524709]),
+oakhart_social_m = L.marker([38.0314250375689, -78.48807223291978]),
+pad_thai_m = L.marker([38.02481230366002, -78.46848020408447]),
+peter_chang_m = L.marker([38.05543382524484, -78.50056586175427]),
+petit_pois_m = L.marker([38.030969240340134, -78.48033249059105]),
+public_fish_m = L.marker([38.03145079000524, -78.48822351942677]),
+revolutionary_soup_m = L.marker([38.03082383888679, -78.48233750408416]),
+south_central_m = L.marker([38.03841532210556, -78.49152958874129]),
+sultan_kebab_m = L.marker([38.0287164503935, -78.48135731942685]),
+tavola_m = L.marker([38.024710303895304, -78.47534271757756]),
+ten_m = L.marker([38.03064239205715, -78.48101040408419]),
+zocalo_m = L.marker([38.03100304379837, -78.48036467709792]);
 
-var OpenStreetMap_France = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+var restaurants = L.layerGroup([al_carbon_m, alley_light_m, beer_run_m, 
+	belle_m, birdhouse_m, bizou_m, 
+	bodos_m, broadcloth_m, cno_m, cafe_frank_m, 
+	conmole_m, crushpad_m, luce_m,
+	fleurie_m, ivy_inn_m, lampo_m,
+	mas_m, milan_indian_m, oakhart_social_m,
+	pad_thai_m, peter_chang_m, petit_pois_m,
+	public_fish_m, revolutionary_soup_m,
+	south_central_m, sultan_kebab_m,
+	tavola_m, ten_m, zocalo_m]);
+
+var union_station_m = L.marker([38.03149962596708, -78.49182173291983]);
+
+var transportation = L.layerGroup([union_station_m]);
+
+var monticello_m = L.marker([38.0085937195138, -78.453211560453]),
+albemarle_county_circuit_court_m = L.marker([38.031647883820426, -78.47745113745796]),
+belmont_m = L.marker([38.024444, -78.4775]),
+albemarle_historical_society_m = L.marker([38.031795495390426, -78.4799721]);
+
+var historical = L.layerGroup([monticello_m, albemarle_county_circuit_court_m, belmont_m, albemarle_historical_society_m]);
+
+var rotunda_m = L.marker([]),
+lawn_m = L.marker([]),
+mad_bowl_m = L.marker([]),
+beta_bridge = L.marker([]),
+memorial_to_enslaved_laborers_m = L.marker([]),
+scott_stadium_m = L.marker([]);
+
+var osmfr = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+});
+
+
+var map = L.map('map', {
+    center: [38.051426013200484, -78.48364145268219],
+    zoom: 11,
+	layers: [osmfr, restaurants,historical, transportation]
+	
+});
+
+var baseMaps = {
+	"openstreetmapfrance":  osmfr
+
+};
+
+var overlayMaps = {
+	"Restaurants": restaurants,
+	"Historical Sites": historical,
+	"Transportation": transportation
+
+}
+
+ var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
